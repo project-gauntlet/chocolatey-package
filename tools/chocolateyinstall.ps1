70921cb7b5a9122d5b2628005fe1fc948becb493
Exidex <16986685+Exidex@users.noreply.github.com>
2024-11-16 21:48:57 +0100

    Gauntlet v11

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -5,9 +5,9 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
   fileType       = 'msi'
-  url64bit       = 'https://github.com/project-gauntlet/gauntlet/releases/download/v10/gauntlet-x86_64-windows.msi'
+  url64bit       = 'https://github.com/project-gauntlet/gauntlet/releases/download/v11/gauntlet-x86_64-windows.msi'
   softwareName   = 'Gauntlet'
-  checksum64     = '76abdec4f662535478cde75ee3b3546d61af1475c979db2603264f3cc4be9458'
+  checksum64     = '1957abe6f67d8b68f929e223afecbbe2b4395a2105365f467ac7d449c65001cf'
   checksumType64 = 'sha256'
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
   validExitCodes = @(0, 3010, 1641)
