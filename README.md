99e8b4a008b5599cc6fba983394000a602ded0c4
Exidex <16986685+exidex@users.noreply.github.com>
2025-01-19 14:06:17 +0100

    Add update-to-latest pipeline

--- README.md
+++ README.md
@@ -23,3 +23,7 @@ https://docs.chocolatey.org/en-us/create/create-packages/#push-your-package
 choco apikey --api-key [API_KEY_HERE] -source https://push.chocolatey.org/
 choco push gauntlet.[VERSION_HERE].0.0.nupkg --source https://push.chocolatey.org/
 ```
+
+OR
+
+Run GitHub Actions workflow
---
+++ updateToLatest.ps1
@@ -0,0 +1,25 @@
+$releaseData = Invoke-RestMethod -Uri "https://api.github.com/repos/project-gauntlet/gauntlet/releases/latest"
+
+$tagName = $releaseData.tag_name
+
+$downloadUrl = $releaseData.assets | Where-Object { $_.browser_download_url -match '\.msi' } | Select-Object -First 1 -ExpandProperty browser_download_url
+
+$ProgressPreference = 'SilentlyContinue'
+Invoke-WebRequest -Uri $downloadUrl -OutFile "gauntlet.msi"
+
+$hash = (Get-FileHash -Path "gauntlet.msi" -Algorithm SHA256).Hash
+
+$version = $tagName.TrimStart('v')
+
+(Get-Content gauntlet.nuspec) -replace '<version>(.+)</version>', "<version>$version.0</version>" | Set-Content gauntlet.nuspec
+
+(Get-Content tools/chocolateyinstall.ps1) -replace 'download/v(.+)/gauntlet', "download/$tagName/gauntlet" | Set-Content tools/chocolateyinstall.ps1
+(Get-Content tools/chocolateyinstall.ps1) -replace "checksum64     = '(.+)'", "checksum64     = '$hash'" | Set-Content tools/chocolateyinstall.ps1
+
+git add gauntlet.nuspec tools/chocolateyinstall.ps1
+git commit -m "Gauntlet $tagName"
+git push
+
+choco pack
+choco apikey --api-key $env:CHOCOLATEY_API_KEY --source "https://push.chocolatey.org/"
+choco push "gauntlet.$version.0.0.nupkg" --source $sourceUrl
